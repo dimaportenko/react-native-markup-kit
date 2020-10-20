77df25ad077699f3856b209edb492cf27bc6f127
Dmytro Portenko <troublesohard@gmail.com>
2020-10-20 12:25:44 +0300

    fix(package config): update package name

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { Constants, View, Text, TouchableOpacity } from '../../src';
+// import { Constants, View, Text, TouchableOpacity } from '../../src';
+import { Constants, View, Text, TouchableOpacity } from 'react-native-markup-kit';
 
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
